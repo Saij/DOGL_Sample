81c629d3139ce12c5424d719d9cc5f810e1e1224
Christoph Friedrich <christoph.friedrich@vonaffenfels.de>
2016-06-17 23:05:04 +0200

    First version

--- DOGL_Sample.ino
+++ DOGL_Sample.ino
@@ -1,9 +1,155 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "DOG_7565R/dog_7565R.h"
+#include "DOG_7565R/font_16x32nums.h"
+#include "DOG_7565R/font_6x8.h"
+#include "DOG_7565R/font_8x16.h"
+#include "DOG_7565R/font_8x8.h"
+#include "DOG_7565R/logo_BLH.h"
 
+dog_7565R display;
+
+//the following port definitions are used by our demo board "EA PCBARDDOG7565"
+int led_red   = 3;
+int led_green = 5;
+int led_blue  = 6;
+
+byte red        = 255;
+byte green      = 255;
+byte blue       = 255;
+byte brightness = 100;
+byte invert     = VIEW_NORMAL;
+
+void initBacklight();
+void rgbBacklight();
+void printHelp();
+void runSerialCommand(char cmd, int16_t value);
+void sampleScreen();
+
+/// State definition for this radio implementation.
+enum COMMAND_STATE {
+  STATE_PARSECOMMAND, // waiting for a new command character.
+  STATE_PARSEINT,     // waiting for digits for the parameter.
+  STATE_EXEC          // executing the command.
+};
+
+COMMAND_STATE state;
+
+void setup() {
+  // open the Serial port
+  Serial.begin(57600);
+  Serial.print("DOGL Sample");
+  delay(500);
+
+  initBacklight();
+  display.initialize(10, 0, 0, 9, 4, DOGL128);
+  display.contrast(63);
+  display.view(VIEW_BOTTOM);
+  display.invert(invert);
+
+  sampleScreen();
+
+  Serial.write('>');
+  state = STATE_PARSECOMMAND;  
+  runSerialCommand('?', 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // some internal static values for parsing the input
+  static char command;
+  static int16_t value;
+  
+  char c;
+  if (Serial.available() > 0) {
+    // read the next char from input.
+    c = Serial.peek();
 
+    if ((state == STATE_PARSECOMMAND) && (c < 0x20)) {
+      // ignore unprintable chars
+      Serial.read();
+    } else if (state == STATE_PARSECOMMAND) {
+      // read a command.
+      command = Serial.read();
+      state = STATE_PARSEINT;
+    } else if (state == STATE_PARSEINT) {
+      if ((c >= '0') && (c <= '9')) {
+        // build up the value.
+        c = Serial.read();
+        value = (value * 10) + (c - '0');
+      } else {
+        // not a value -> execute
+        runSerialCommand(command, value);
+        command = ' ';
+        state = STATE_PARSECOMMAND;
+        value = 0;
+      }
+    }
+  }
+}
+
+void initBacklight() {
+  pinMode(led_blue,  OUTPUT);
+  pinMode(led_green, OUTPUT);
+  pinMode(led_red,   OUTPUT);
+  rgbBacklight();
+}
+
+void rgbBacklight() {
+  analogWrite(led_red,    red * brightness / 100);
+  analogWrite(led_green,  green * brightness / 100);
+  analogWrite(led_blue,   blue * brightness / 100);
+}
+
+void printHelp() {
+  Serial.println();
+  Serial.println("? Help");
+  Serial.println("l Set brightness (0 to 100)");
+  Serial.println("c Set contrast (0 to 63)");
+  Serial.println("w Wipe display");
+  Serial.println("n Normal (Bottom) view");
+  Serial.println("t Top View");
+  Serial.println("r Backlight color red");
+  Serial.println("g Backlight color green");
+  Serial.println("b Backlight color blue");
+  Serial.println("i Toggle invert");
+}
+
+void sampleScreen() {
+  display.clear();  //clear whole display
+  display.picture(0,0,ea_logo);
+  display.string(71,0,font_8x16,"DOGL128");
+  display.rectangle(71,2,127,2,0x03);
+  display.string(0,3,font_6x8,"-3.3V single supply");
+  display.string(0,4,font_6x8,"-ST7565R controller");
+  display.string(0,5,font_6x8,"-different backlights");
+  display.string(0,6,font_6x8,"-extrem low power");
+  display.string(0,7,font_8x8,"lcd-module.com");
+}
+
+void runSerialCommand(char cmd, int16_t value) {
+  if (cmd == '?') {
+    printHelp();
+  } else if (cmd == 'l') {
+    brightness = constrain(value, 0, 100);
+    rgbBacklight();
+  } else if (cmd == 'c') {
+    display.contrast(value);
+  } else if (cmd == 'w') {
+    display.clear();
+    sampleScreen();
+  } else if (cmd == 'n') {
+    display.view(VIEW_BOTTOM);
+  } else if (cmd == 't') {
+    display.view(VIEW_TOP);
+  } else if (cmd == 'r') {
+    red = constrain(value, 0, 255);
+    rgbBacklight();
+  } else if (cmd == 'g') {
+    green = constrain(value, 0, 255);
+    rgbBacklight();
+  } else if (cmd == 'b') {
+    blue = constrain(value, 0, 255);
+    rgbBacklight();
+  } else if (cmd == 'i') {
+    invert = (invert == VIEW_NORMAL ? VIEW_INVERT : VIEW_NORMAL);
+    display.invert(invert);
+  }
 }
---
+++ DOG_7565R/examples/dogm132_RGB_backlight/dogm132_RGB_backlight.ino
@@ -0,0 +1,92 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <dog_7565R.h>
+#include <font_16x32nums.h>
+#include <font_6x8.h>
+#include <font_8x16.h>
+#include <font_8x8.h>
+#include <logo_small_BLH.h>
+
+/*Available functions in dog_7565R Libraray:
+  void initialize  (byte p_cs, byte p_si, byte p_clk, byte p_a0, byte p_res, byte type);
+  void clear       (void);
+  void contrast    (byte contr);
+  void view        (byte direction);
+  void string      (byte column, byte page, const byte *font_adress, const char *str);
+  void rectangle   (byte start_column, byte start_page, byte end_column, byte end_page, byte pattern);
+  void picture     (byte column, byte page, const byte *pic_adress);
+*/
+
+dog_7565R DOG;
+
+//the following port definitions are used by our demo board "EA PCBARDDOG7565"
+int led       = 3;
+int led_red   = 3;
+int led_green = 5;
+int led_blue  = 6;
+void init_backlight(boolean mono);
+void mono_backlight(byte brightness);
+void rgb_backlight(byte red, byte green, byte blue);
+
+
+void sample_screen(void);
+
+//initialize the backlight and the display
+void setup()
+{
+  init_backlight(false); //use RGB backlight in this sample code. Please change it to your configuration
+  DOG.initialize(10,0,0,9,4,DOGM132);   //SS = 10, 0,0= use Hardware SPI, 9 = A0, 4 = RESET, EA DOGM128-6 (=128x64 dots)
+}
+
+//create a sample sceen content
+void sample_screen(void)
+{
+  DOG.clear();  //clear whole display
+  DOG.picture(0,0,ea_logo_small);
+  DOG.string(32,0,font_8x8,"DOGM132-5");      //font size 8x16 fifth page         
+  DOG.rectangle(32,1,103,1,0x03);             //show small line (filled pattern = 0x03), to the left and right of EA DOGM132-5  
+  DOG.string(36,2,font_6x8,"3.3V single sup.");
+  DOG.string(36,3,font_6x8,"ST7565R");
+}
+
+//main loop
+void loop()
+{
+  rgb_backlight(random(255),random(255),random(255));   //random backlight color (red, green, blue)
+  DOG.view(VIEW_BOTTOM);      //default viewing direction
+  sample_screen();            //show content
+  delay(2000);
+  rgb_backlight(random(255),random(255),random(255));   //random backlight color
+  DOG.view(VIEW_TOP);         //alternate viewing direction
+  sample_screen();
+  delay(2000);
+}
+
+//The following functions controll the backlight with a PWM. Not needed for the display content
+void init_backlight(boolean mono)
+{
+  if(mono) //EA LED55X31-G, EA LED55X31-W, EA LED55X31-B, EA LED55X31-A, EA LED55X31-R
+  {
+    pinMode(led,  OUTPUT);
+    mono_backlight(255);
+  }
+  else  //EA LED55X31-RGB
+  {
+    pinMode(led_blue,  OUTPUT);
+    pinMode(led_green, OUTPUT);
+    pinMode(led_red,   OUTPUT);
+    rgb_backlight(255,0,0);
+  }
+}
+//Use this funtion for monochrome backlight
+void mono_backlight(byte brightness)
+{
+  analogWrite(led, brightness);  
+}
+//Use this function for RGB backlight
+void rgb_backlight(byte red, byte green, byte blue)
+{
+  analogWrite(led_red,  red);  
+  analogWrite(led_green, green);  
+  analogWrite(led_blue,   blue);  
+}
